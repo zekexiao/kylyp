1c10ca470c9ed98902e583d74418f1fd36236af9
kriry <krircc@gmail.com>
2017-09-12 10:42:00 +0800

    添加新闻，板块类别导航，添加博客子功能模块

--- public/js/tabs-index.js
+++ public/js/tabs-index.js
@@ -1,60 +1,27 @@
-// onload=function(){
-//     var links = document.getElementById("top").getElementsByTagName('li')
-//     links_len=links.length;
-//     for(var i=0; i<links_len; i++){
-//         links[i].onmouseover=function(){
-//             clearInterval(iIntervalId);
-//             this.onmouseout=function(){
-//                 iIntervalId = setInterval(setTab('one',1),ScrollTime);;
-//             }
-//         }
-//     }
-// }
-// function setTab(name,cursel){
-//     cursel_0=cursel;
-//     for(var i=1; i<=links_len; i++){
-//         var menu = document.getElementById(name+i);
-//         var menudiv = document.getElementById("con_"+name+"_"+i);
-//         if(i==cursel){
-//             menu.className="off";
-//             menudiv.style.display="block";
-//         }
-//         else{
-//             menu.className="";
-//             menudiv.style.display="none";
-//         }
-//     }
-// }
-
-function setTab(name,cursel){
-	cursel_0=cursel;
-	for(var i=1; i<=links_len; i++){
-		var menu = document.getElementById(name+i);
-		var menudiv = document.getElementById("con_"+name+"_"+i);
-		if(i==cursel){
-			menu.className="off";
-			menudiv.style.display="block";
-		}
-		else{
-			menu.className="";
-			menudiv.style.display="none";
-		}
-	}
-}
-
-var name_0='one';
-var cursel_0=1;
-var ScrollTime=3000;//循环周期（毫秒）
-var links_len,iIntervalId;
 onload=function(){
-	var links = document.getElementById("top").getElementsByTagName('li')
-	links_len=links.length;
-	for(var i=0; i<links_len; i++){
-		links[i].onmouseover=function(){
-			clearInterval(iIntervalId);
-			this.onmouseout=function(){
-				iIntervalId = setInterval(Next,ScrollTime);;
-			}
-		}
-	}
+    var links = document.getElementById("top").getElementsByTagName('li')
+    links_len=links.length;
+    for(var i=0; i<links_len; i++){
+        links[i].onmouseover=function(){
+            clearInterval(iIntervalId);
+            this.onmouseout=function(){
+                iIntervalId = setInterval(setTab('one',1),ScrollTime);;
+            }
+        }
+    }
+}
+function setTab(name,cursel){
+    cursel_0=cursel;
+    for(var i=1; i<=links_len; i++){
+        var menu = document.getElementById(name+i);
+        var menudiv = document.getElementById("con_"+name+"_"+i);
+        if(i==cursel){
+            menu.className="off";
+            menudiv.style.display="block";
+        }
+        else{
+            menu.className="";
+            menudiv.style.display="none";
+        }
+    }
 }
